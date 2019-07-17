8617b898004b5c928bf063801642c17e75a4ea9a
hck <kevin_hck@foxmail.com>
2019-07-17 21:10:48 +0800

    add some wxutils methods

--- miniprogram-template/utils/wxutils.js
+++ miniprogram-template/utils/wxutils.js
@@ -1,10 +1,12 @@
+import { isStrNull } from './utils';
+
 const modal = (tip, showCancel) => {
   return new Promise((resolve, reject) => {
     wx.showModal({
       title: '提示',
       content: tip || '是否确认该操作',
       showCancel: showCancel || true,
-      success: res => {
+      success(res) {
         if (res.confirm) {
           console.log('用户点击确定');
           resolve();
@@ -17,15 +19,128 @@ const modal = (tip, showCancel) => {
   });
 };
 
+const wxGetShareInfo = shareTicket => {
+  return new Promise((resolve, reject) => {
+    wx.getShareInfo({
+      shareTicket,
+      complete(res) {
+        resolve(res);
+      },
+      fail(err) {
+        reject(err);
+      }
+    });
+  });
+};
+
 const noneToast = tip => {
+  const isTipValid = !isStrNull(tip) && tip && typeof tip === 'string';
+  if (isTipValid) {
+    wx.showToast({
+      title: tip && typeof tip === 'string' ? tip : '成功',
+      icon: 'none',
+      duration: 2000
+    });
+  }
+};
+
+const noneToastNotNull = tip => {
+  if (typeof tip === 'string' && !isStrNull(tip)) {
+    wx.showToast({
+      title: tip,
+      icon: 'none',
+      duration: 2000
+    });
+  }
+};
+
+const successToast = (tip) => {
   wx.showToast({
     title: tip || '成功',
-    icon: 'none',
-    duration: 2000
+    icon: 'success',
+    duration: 1000
+  });
+};
+
+const redirect = url => {
+  wx.redirectTo({
+    url
+  });
+};
+
+const navigate = url => {
+  wx.navigateTo({
+    url
+  });
+};
+
+const switchTab = url => {
+  wx.switchTab({
+    url
+  });
+};
+
+const reLaunch = url => {
+  wx.reLaunch({
+    url
+  });
+};
+
+const isNull = obj => {
+  return typeof obj === 'undefined' || obj === null;
+};
+
+const showLoading = (title, mask) => {
+  wx.showLoading({
+    title,
+    mask: isNull(mask) ? true : mask
+  });
+};
+
+const chooseImg = remainPicCount => {
+  return new Promise((resolve, reject) => {
+    wx.chooseImage({
+      count: remainPicCount,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        resolve(res.tempFilePaths);
+      },
+      fail() {
+        reject();
+      }
+    });
+  });
+};
+
+const chooseVideo = () => {
+  return new Promise((resolve, reject) => {
+    wx.chooseVideo({
+      sourceType: ['album','camera'],
+      maxDuration: 60,
+      camera: ['front', 'back'],
+      success(res) {
+        resolve(res);
+      },
+      fail() {
+        reject();
+      }
+    });
   });
 };
 
 module.exports = {
   modal,
-  noneToast
+  noneToast,
+  noneToastNotNull,
+  successToast,
+  redirect,
+  switchTab,
+  navigate,
+  reLaunch,
+  showLoading,
+  wxGetShareInfo,
+  chooseImg,
+  chooseVideo
 };
+
